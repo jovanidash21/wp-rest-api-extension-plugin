addffc1e93f27bd393a49bdb36d4c70626765d00
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-01 22:11:13 +0800

    Initial Commit

---
+++ wp-rest-api-extension.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP REST API Extension
+ * Plugin URI: https://jovaniwarguez.wordpress.com
+ * Description: A plugin that extends WordPress REST API with new routes
+ * Version: 0.0.0
+ * Author: Jovani Warguez
+ * Author URI: https://jovaniwarguez.wordpress.com
+ * Text Domain: wp-rest-api-extension-plugin
+ */
