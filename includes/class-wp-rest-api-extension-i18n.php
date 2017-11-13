66ef5b1bbbc35016f67017017045d7c1cf15d008
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-11-13 22:02:44 +0800

    Add Plugin Internationalization And Loader File

---
+++ includes/class-wp-rest-api-extension-i18n.php
@@ -0,0 +1,15 @@
+<?php
+
+class WP_REST_API_Extension_i18n {
+
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'wp-rest-api-extension',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+}
