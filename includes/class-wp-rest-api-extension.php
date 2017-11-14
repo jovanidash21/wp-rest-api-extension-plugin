0594ef9f4294f49015176db2badf082160610901
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-11-14 10:15:11 +0800

    Update Includes Files

---
+++ includes/class-wp-rest-api-extension.php
@@ -0,0 +1,77 @@
+<?php
+
+class WP_REST_API_Extension {
+
+	protected $loader;
+	protected $plugin_name;
+	protected $version;
+
+	public function __construct() {
+		if ( defined( 'PLUGIN_NAME_VERSION' ) ) {
+			$this->version = PLUGIN_NAME_VERSION;
+		} else {
+			$this->version = '1.0.0';
+		}
+		$this->plugin_name = 'wp-rest-api-extension';
+
+		$this->load_dependencies();
+		$this->set_locale();
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
+
+	}
+
+	private function load_dependencies() {
+
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-rest-api-extension-loader.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-rest-api-extension-i18n.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wp-rest-api-extension-admin.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wp-rest-api-extension-public.php';
+
+		$this->loader = new WP_REST_API_Extension_Loader();
+
+	}
+
+	private function set_locale() {
+
+		$plugin_i18n = new WP_REST_API_Extension_i18n();
+
+		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+
+	}
+
+	private function define_admin_hooks() {
+
+		$plugin_admin = new WP_REST_API_Extension_Admin( $this->get_plugin_name(), $this->get_version() );
+
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+
+	}
+
+	private function define_public_hooks() {
+
+		$plugin_public = new WP_REST_API_Extension_Public( $this->get_plugin_name(), $this->get_version() );
+
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+
+	}
+
+	public function run() {
+		$this->loader->run();
+	}
+
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+
+	public function get_loader() {
+		return $this->loader;
+	}
+
+	public function get_version() {
+		return $this->version;
+	}
+
+}
