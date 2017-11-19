4209501ab5708fbc8717cc688707599778450e1a
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-11-19 11:12:55 +0800

    Update Settings Page

--- admin/partials/wp-rest-api-extension-admin-display.php
+++ admin/partials/wp-rest-api-extension-admin-display.php
@@ -1,7 +1,19 @@
-<?php
-
-?>
-
 <div class="wrap">
   <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
+
+  <div id="poststuff">
+    <div id="post-body" class="metabox-holder columns-1">
+      <div class="meta-box-sortables ui-sortable">
+        <div class="postbox">
+          <form method="post" name="wp_rest_api_extension_settings" action="options.php">
+            
+
+            <div class="inside">
+              <?php submit_button('Save all changes', 'primary','submit', TRUE); ?>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
