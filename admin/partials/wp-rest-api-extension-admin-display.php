fb297724bd07f1e7e8830be6a4c5f21947dca607
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-11-19 11:15:27 +0800

    Add Post Types List Checkbox

--- admin/partials/wp-rest-api-extension-admin-display.php
+++ admin/partials/wp-rest-api-extension-admin-display.php
@@ -6,7 +6,44 @@
       <div class="meta-box-sortables ui-sortable">
         <div class="postbox">
           <form method="post" name="wp_rest_api_extension_settings" action="options.php">
-            
+            <h2 class="hndle"><?php echo 'Add Next And Previous Links'; ?></h2>
+            <div class="inside">
+              <h4><?php echo 'Post types'; ?></h4>
+
+              <?php
+                $post_types = get_post_types(
+                  array(
+                    '_builtin' => false,
+                    'public' => true
+                  ), 'object'
+                );
+              ?>
+              <fieldset>
+                <legend class="screen-reader-text"><span>Add Next And Previous Links</span></legend>
+                <label for="<?php echo $this->plugin_name; ?>-next-prev-links-page">
+                  <input type="checkbox" id="<?php echo $this->plugin_name; ?>-next-prev-links-page" name="<?php echo $this->plugin_name; ?> [next-prev-links-page]" value="1"/>
+                    <span><?php esc_attr_e('Page', $this->plugin_name); ?></span>
+                </label>
+              </fieldset>
+              <fieldset>
+                <legend class="screen-reader-text"><span>Add Next And Previous Links</span></legend>
+                <label for="<?php echo $this->plugin_name; ?>-next-prev-links-post">
+                  <input type="checkbox" id="<?php echo $this->plugin_name; ?>-next-prev-links-post" name="<?php echo $this->plugin_name; ?> [next-prev-links-post]" value="1"/>
+                    <span><?php esc_attr_e('Post', $this->plugin_name); ?></span>
+                </label>
+              </fieldset>
+              <?php if ( ! empty( $post_types ) ) : ?>
+                <?php foreach ( $post_types as $post_type ) : ?>
+                  <fieldset>
+                    <legend class="screen-reader-text"><span>Add Next And Previous Links</span></legend>
+                    <label for="<?php echo $this->plugin_name; ?>-next-prev-links-<?php echo $post_type->name ?>">
+                      <input type="checkbox" id="<?php echo $this->plugin_name; ?>-next-prev-links-<?php echo $post_type->name ?>" name="<?php echo $this->plugin_name; ?> [next-prev-links-<?php echo $post_type->name ?>]" value="1"/>
+                        <span><?php esc_attr_e($post_type->label, $this->plugin_name); ?></span>
+                    </label>
+                  </fieldset>
+                <?php endforeach; ?>
+              <?php endif;  ?>
+            </div>
 
             <div class="inside">
               <?php submit_button('Save all changes', 'primary','submit', TRUE); ?>
