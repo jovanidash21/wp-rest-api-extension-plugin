9d88348572dabb4f7f36c83ee3401093cbbe720c
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-11-21 19:30:21 +0800

    Add Registered Nav Menus List Checkbox

--- admin/partials/wp-rest-api-extension-admin-display.php
+++ admin/partials/wp-rest-api-extension-admin-display.php
@@ -6,6 +6,20 @@
       <div class="meta-box-sortables ui-sortable">
         <div class="postbox">
           <form method="post" name="wp_rest_api_extension_settings" action="options.php">
+            <h2 class="hndle"><?php echo 'Add Registered Nav Menu API'; ?></h2>
+            <div class="inside">
+              <?php $menus = get_registered_nav_menus(); ?>
+              <?php foreach ( $menus as $location => $description ) : ?>
+                <fieldset>
+                  <legend class="screen-reader-text"><span>Add Registered nav Menu API</span></legend>
+                  <label for="<?php echo $this->plugin_name; ?>-registered-nav-menu-<?php echo $location ?>">
+                    <input type="checkbox" id="<?php echo $this->plugin_name; ?>-registered-nav-menu-<?php echo $location ?>" name="<?php echo $this->plugin_name; ?> [registered-nav-menu-<?php echo $location ?>]" value="1"/>
+                      <span><?php esc_attr_e($description, $this->plugin_name); ?></span>
+                  </label>
+                </fieldset>
+              <?php endforeach;  ?>
+            </div>
+
             <h2 class="hndle"><?php echo 'Add Next And Previous Links'; ?></h2>
             <div class="inside">
               <h4><?php echo 'Post types'; ?></h4>
