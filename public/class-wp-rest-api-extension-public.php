6f509ea0a036393cb2ce1a044d763dd069449a34
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-11-29 16:22:07 +0800

    Update Plugin Files

--- public/class-wp-rest-api-extension-public.php
+++ public/class-wp-rest-api-extension-public.php
@@ -6,22 +6,16 @@ class WP_REST_API_Extension_Public {
 	private $version;
 
 	public function __construct( $plugin_name, $version ) {
-
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-
 	}
 
 	public function enqueue_styles() {
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-rest-api-extension-public.css', array(), $this->version, 'all' );
-
 	}
 
 	public function enqueue_scripts() {
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-rest-api-extension-public.js', array( 'jquery' ), $this->version, false );
-
 	}
 
 }
